03a54f4f6a901ca4166a9329c2880583a45d210f
sfigiel <szymon.figiel@outlook.com>
2020-09-01 19:27:41 +0200

    add tests for delete method

--- api_mirumee/books/tests/test_endpoints.py
+++ api_mirumee/books/tests/test_endpoints.py
@@ -25,6 +25,11 @@ class AuthorTestV1(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(Authors.objects.count(), 1)
 
+    def test_delete_author(self):
+        response = self.client.delete(f"http://localhost:8000/api/v1/authors/f40f097a-36fc-47b8-bcea-5f3f25a919df/")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Authors.objects.count(), 0)
+
 
 class BooksTestV1(APITestCase):
     def setUp(self):
@@ -57,6 +62,11 @@ class BooksTestV1(APITestCase):
         response = self.client.get("http://localhost:8000/api/v1/books/Python Image Processing Cookbook/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_delete_book(self):
+        response = self.client.delete(f"http://localhost:8000/api/v1/books/Python OpenCV/")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Books.objects.count(), 1)
+
 
 class RatesTestV1(APITestCase):
     def setUp(self):
@@ -68,7 +78,7 @@ class RatesTestV1(APITestCase):
                              genres="Education")
         Rates.objects.create(book_id=book_1,
                              rate=4,
-                             text="Education")
+                             text="Not bad")
 
     def test_create_rate(self):
         data = {"book_id": "3fec8f9d-b439-4d64-b219-21b77d017850",
@@ -83,3 +93,9 @@ class RatesTestV1(APITestCase):
         response = self.client.get("http://localhost:8000/api/v1/rates/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(Rates.objects.count(), 1)
+
+    def test_delete_rate(self):
+        rate_id = Rates.objects.get(text="Not bad").id
+        response = self.client.delete(f"http://localhost:8000/api/v1/rates/{rate_id}/")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Rates.objects.count(), 0)
